3c366795e7d47fe7bace9d1cd496ae3c20c175be
Nathan Buckner <nathan.buckner@rackspace.com>
2015-06-08 02:04:42 -0500

    update

--- cloudcafe/identity/client.py
+++ cloudcafe/identity/client.py
@@ -1,6 +1,12 @@
+"""
+This is an Identity Client
+"""
+
+import json
 from cafe.engine.http.client import HTTPClient
 
 class IdentityClient(HTTPClient):
+	"""Identity Client Class for Rackspace"""
 	def __init__(self, url, token):
 		super(IdentityClient, self).__init__()
 		self.url = url
@@ -9,4 +15,10 @@ class IdentityClient(HTTPClient):
 			"Accept": "application/json",
 			"Content-Type": "application/json"}
 
-	
+	def authenticate(self, username, password):
+		"""Authenticates with user and password and returns a response obj"""
+		data = json.dumps({"auth": {"passwordCredentials": {
+			"username": username, "password": password}}})
+		url = "{url}/tokens".format(url=self.url)
+		return self.post(url, data=data)
+
